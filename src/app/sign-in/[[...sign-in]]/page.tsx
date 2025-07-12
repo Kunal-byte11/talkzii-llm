2d8919a9265038ea8df3908ce00aab824b5c7f93
Kunal-byte11 <kunaldubey26@outlook.com>
2025-07-12 17:59:04 +0530

    Delete src/app/sign-in/[[...sign-in]] directory

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,6 +0,0 @@
-# Supabase (Keep these if you were using Supabase for database)
-# NEXT_PUBLIC_SUPABASE_URL=your_supabase_url_here
-# NEXT_PUBLIC_SUPABASE_ANON_KEY=your_supabase_anon_key_here
-
-# Google Analytics (if you set it up)
-# NEXT_PUBLIC_GA_MEASUREMENT_ID=G-GC1HKJBY12
